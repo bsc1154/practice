a55032e7f8ac70fb4739a9070ce9661861bd2f01
bsc1154 <khl1154@naver.com>
2022-02-23 19:14:36 +0900

    Add files via upload

---
+++ javaPractice/Person.java
@@ -0,0 +1,15 @@
+package com.example.stream;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Person {
+    private String name;
+    private int age;
+    private String phoneNumber;
+}
---
+++ javaPractice/StreamApplicationTests.java
@@ -0,0 +1,69 @@
+package com.example.stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.print.attribute.standard.NumberOfInterveningJobs;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+public class StreamApplicationTests {
+        public List<Person> personList = new ArrayList<Person>();
+        //personList.add(new Person("짱구",23,"010-1234-1234"));
+        //personList.add(new Person("유리",24,"010-2341-2341"));
+        //personList.add(new Person("철수",29,"010-3412-3412"));
+        //personList.add(new Person("맹구",25,null));
+
+    @DisplayName("streamTest1입니다.")
+    @Test
+    void streamTest1(){
+
+
+        //Map<String, Integer> personMap = personList.stream().collect(Collectors.toMap(Person::getName,Person::getAge));
+        //1. stream으로 이름과 전화 번호를 출력하라(null check)
+        //2. stream으로 이름과 요소 전체를 출력하라
+        //3. stream으로 나이가 25이상만 출력하라
+        //4. stream으로 이름이 짱구만 출력하라
+        //5. stream으로 상위 1개만 출력하라
+        //6. stream으로 정렬하라
+        //7.
+        personList.add(new Person("짱구",23,"010-1234-1234"));
+        //personList.add(new Person("유리",24,"010-2341-2341"));
+        //personList.add(new Person("철수",29,"010-3412-3412"));
+        //personList.add(new Person("맹구",25,null));
+        //Map<String, String> personMap = personList.stream().collect(Collectors.toMap(Person::getName,Person::getPhoneNumber));
+        Map<String,Object> personMap2 = personList.stream().collect(Collectors.toMap(Person::getName,Function.identity()));
+        System.out.println(personMap2);
+
+
+    }
+
+    @DisplayName("스트림으로 나이가 25이상만 출력.")
+    @Test
+    void streamTest2(){
+        personList.add(new Person("짱구",23,"010-1234-1234"));
+        personList.add(new Person("유리",24,"010-2341-2341"));
+        personList.add(new Person("철수",29,"010-3412-3412"));
+        personList.add(new Person("맹구",25,null));
+        Map<String,Person> personMap2 = personList.stream().filter(person->person.getAge()>25).collect(Collectors.toMap(Person::getName,Function.identity()));
+        System.out.println(personMap2);
+    }
+
+    @DisplayName("스트림으로 이름이 짱구인 사람만 출력.")
+    @Test
+    void streamTest3(){
+        personList.add(new Person("짱구",23,"010-1234-1234"));
+        personList.add(new Person("유리",24,"010-2341-2341"));
+        personList.add(new Person("철수",29,"010-3412-3412"));
+        personList.add(new Person("맹구",25,null));
+
+        Map<String,String> personMap2 = personList.stream().filter(person->person.getPhoneNumber()!=null&&person.getName()!=null).collect(Collectors.toMap(Person::getName,Person::getPhoneNumber));
+        System.out.println(personMap2);
+    }
+}
