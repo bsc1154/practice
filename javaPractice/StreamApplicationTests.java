6c5de33cdf37f28478d2a80f24a9f8fee9778838
bsc1154 <khl1154@naver.com>
2022-02-24 02:21:33 +0900

    Update StreamApplicationTests.java

--- javaPractice/StreamApplicationTests.java
+++ javaPractice/StreamApplicationTests.java
@@ -1,24 +1,21 @@
-package com.example.stream;
+package com.example.demo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.print.attribute.standard.NumberOfInterveningJobs;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @SpringBootTest
-public class StreamApplicationTests {
-        public List<Person> personList = new ArrayList<Person>();
-        //personList.add(new Person("짱구",23,"010-1234-1234"));
-        //personList.add(new Person("유리",24,"010-2341-2341"));
-        //personList.add(new Person("철수",29,"010-3412-3412"));
-        //personList.add(new Person("맹구",25,null));
+class DemoApplicationTests {
+
+    public List<Person> personList = new ArrayList<Person>();
+    //personList.add(new Person("짱구",23,"010-1234-1234"));
+    //personList.add(new Person("유리",24,"010-2341-2341"));
+    //personList.add(new Person("철수",29,"010-3412-3412"));
+    //personList.add(new Person("맹구",25,null));
 
     @DisplayName("streamTest1입니다.")
     @Test
@@ -51,7 +48,7 @@ public class StreamApplicationTests {
         personList.add(new Person("유리",24,"010-2341-2341"));
         personList.add(new Person("철수",29,"010-3412-3412"));
         personList.add(new Person("맹구",25,null));
-        Map<String,Person> personMap2 = personList.stream().filter(person->person.getAge()>25).collect(Collectors.toMap(Person::getName,Function.identity()));
+        Map<String,Person> personMap2 = personList.stream().filter(person->person.getAge()>25).collect(Collectors.toMap(Person::getName, Function.identity()));
         System.out.println(personMap2);
     }
 
@@ -63,7 +60,63 @@ public class StreamApplicationTests {
         personList.add(new Person("철수",29,"010-3412-3412"));
         personList.add(new Person("맹구",25,null));
 
-        Map<String,String> personMap2 = personList.stream().filter(person->person.getPhoneNumber()!=null&&person.getName()!=null).collect(Collectors.toMap(Person::getName,Person::getPhoneNumber));
+        Map<String,String> personMap2 = personList.stream().filter(person->"짱구".equals(person.getName())).collect(Collectors.toMap(Person::getName,Person::getPhoneNumber));
         System.out.println(personMap2);
     }
+
+    @DisplayName("상위1개만 출력")
+    @Test
+    void streamTest4(){
+        personList.add(new Person("짱구",23,"010-1234-1234"));
+        personList.add(new Person("유리",24,"010-2341-2341"));
+        personList.add(new Person("철수",29,"010-3412-3412"));
+        personList.add(new Person("맹구",25,null));
+
+        Map<String,String> personMap3 = personList.stream().limit(1).collect(Collectors.toMap(Person::getName,Person::getPhoneNumber));
+        System.out.println(personMap3);
+    }
+
+    @DisplayName("정렬")
+    @Test
+    void streamTest5(){
+        personList.add(new Person("짱구",23,"010-1234-1234"));
+        personList.add(new Person("유리",24,"010-2341-2341"));
+        personList.add(new Person("철수",29,"010-3412-3412"));
+        personList.add(new Person("맹구",25,null));
+
+        Map<String,String> personMap3 = personList.stream().collect(Collectors.toMap(Person::getName,Person::getPhoneNumber));
+        System.out.println(personMap3);
+    }
+
+    @DisplayName("리듀스")
+    @Test
+    void streamTest6(){
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(5);
+        list.add(4);
+        list.add(2);
+        list.add(1);
+        list.add(6);
+        list.add(7);
+
+        Integer personMap3 = list.stream().mapToInt(x -> x).sum();
+        System.out.println(personMap3);
+    }
+
+    @DisplayName("정렬")
+    @Test
+    void streamTest7(){
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("짱구", 25, "010-1234-1234"));
+        personList.add(new Person("유리", 24, "010-2341-2341"));
+        personList.add(new Person("맹구", 23, "010-3412-3412"));
+        personList.add(new Person("훈이", 26, "010-4123-4123"));
+
+        // 맹구, 유리, 짱구, 훈이
+        personList.stream()
+                .sorted(Comparator.comparing(Person::getAge));
+
+        System.out.println(personList);
+    }
+
 }
